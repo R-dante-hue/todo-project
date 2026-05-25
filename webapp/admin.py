9085b063e60d31221c398a39f2edd22a99beb55c
Roman <romanchevroman2007@gmail.com>
2026-05-25 14:54:39 +0600

    Add Task model with status choices and register in admin panel

---
+++ webapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['description', 'status', 'due_date', 'created_at']
+    list_filter = ['status']
